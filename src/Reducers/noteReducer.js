4d3f9ddc9d965669a48190e3492aa391cf2ae22a
Andrew Crawford <andrew.crawford.za@gmail.com>
2018-03-01 19:58:08 +0200

    Fixx the bug of the previous commit
    - Add  more structure to theNote reducer
    - tidy the actionTypes.js file

--- src/Reducers/noteReducer.js
+++ src/Reducers/noteReducer.js
@@ -1,34 +1,54 @@
 import actionTypes from '../actionTypes';
 
-const noteReducer = (
-  state = {
-    [
-        {
-          id: 1,
-          text: 'this is text 1',
-          selected: false,
-        },
-        {
-          id: 2,
-          text: 'this is text 2',
-          selected: false,
-        },
-        {
-          id: 3,
-          text: 'this is text 3',
-          selected: false,
-        },
-        {
-          id: 4,
-          text: 'this is text 4',
-          selected: false,
-        },
-      ]
+/**
+ * ----------------------------------
+ * The Note Reducer file.
+ * ----------------------------------
+ */
+
+/**
+ * The initial state for the noteReducer
+ */
+const initialState = [
+  {
+    id: 1,
+    text: 'this is text 1',
+    selected: false,
   },
-  action,
-) => {
+  {
+    id: 2,
+    text: 'this is text 2',
+    selected: false,
+  },
+  {
+    id: 3,
+    text: 'this is text 3',
+    selected: false,
+  },
+  {
+    id: 4,
+    text: 'this is text 4',
+    selected: false,
+  },
+];
+
+/**
+ * The Note reducer.
+ * This will hold all the data for notes.
+ * @param {array} state
+ * Example of how data will look.
+ *  [{
+ *   id: 4,
+ *   text: 'this is text 4',
+ *   selected: false,
+ *   }],
+ * @param {string} action the actions afre all found here 'actionTypes.js'
+ */
+const noteReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.NOTE_CREATE:
+      // A new note must be created
+      // TODO: Still TODO
       console.log('actionTypes.NOTE_CREATE');
       // state = {
       //     ...state,
@@ -37,7 +57,9 @@ const noteReducer = (
       break;
 
     case actionTypes.NOTE_SELECTED_CHANGE:
+      // The selected property of the matched not must be toggled.
       console.log('actionTypes.NOTE_SELECTED_CHANGE');
+      // TODO: Still TODO
       // state = {
       //     ...state,
       //     age: action.payload
--- src/actionTypes.js
+++ src/actionTypes.js
@@ -1,6 +1,7 @@
 // These are the actions type;
+const actionTypes = {
+  NOTE_CREATE: 'NOTE_CREATE',
+  NOTE_SELECTED_CHANGE: 'NOTE_SELECTED_CHANGE',
+};
 
-export default actionTypes = {
-    NOTE_CREATE: 'NOTE_CREATE',
-    NOTE_SELECTED_CHANGE:'NOTE_SELECTED_CHANGE'
-}
+export default actionTypes;
