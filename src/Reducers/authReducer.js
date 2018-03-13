046e1a1fdb002eab3db99b5cf3d60ca44c833cb0
Andrew Crawford <andrew.crawford.za@gmail.com>
2018-03-13 16:59:50 +0200

    Begin the authReducer.

---
+++ src/Reducers/authReducer.js
@@ -0,0 +1,47 @@
+import actionTypes from '../constants/actionTypes';
+import { deepClone } from '../utils/general';
+
+const defaultState = {
+  login: {
+    profile: {},
+    loggedIn: false,
+    isLoading: false,
+    error: {
+      isError: false,
+      isErrorMessage: '',
+    } 
+  }
+};
+
+const authReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case actionTypes.AUTH_LOGIN_IS_LOADING:
+      state = deepClone(state);
+      state.login.isLoading = action.payload.isLoading;
+      break;
+
+    case actionTypes.AUTH_LOGIN_ERROR:
+      state = deepClone(state);
+      state.error.isError = action.payload.isError;
+      state.error.isErrorMessage = action.payload.isErrorMessage;
+      break;      
+      
+    case actionTypes.AUTH_LOGIN_SUCCESS_WITH_DATA:
+      state = deepClone(state);
+      state.login.loggedIn = action.payload.loggedIn;
+      state.login.profile = action.payload.profile;
+      break;
+      
+    default:
+      console.log('No actions match');
+  }
+}
+    
+  console.log('itemsReducer', [state, action]);
+  return state;
+};
+
+export default authReducer;
+
+
+
